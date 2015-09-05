d411da4277e95b4dc87f0b7f8513ed1bebfc45c0
evgeniy <evgeniy@takeit.to>
2015-09-05 06:04:23 +0300

    add method getPageContent

--- Analyzer.java
+++ Analyzer.java
@@ -1,5 +1,14 @@
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 class Analyzer {
     private static Analyzer instance;
@@ -17,5 +26,33 @@ class Analyzer {
         Pattern pattern = Pattern.compile(regex);
         Matcher m = pattern.matcher(url);
           return m.matches();
+    }
+
+    public static String getPageContent (String pageUrl) {
+
+      URL url;
+      StringBuffer resultString = new StringBuffer();
+
+  		try {
+  		    url = new URL(pageUrl);
+  		  	URLConnection conn = url.openConnection();
+
+   			  BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+  			  String inputLine;
+
+  			  while ((inputLine = br.readLine()) != null) {
+  				      resultString.append(inputLine);
+  			  }
+
+   			  br.close();
+  		} catch (MalformedURLException e) {
+  			   System.out.println(e.getMessage());
+            return null;
+  		} catch (IOException e) {
+           System.out.println(e.getMessage());
+            return null;
+  		}
+          return resultString.toString();
     }
 }
