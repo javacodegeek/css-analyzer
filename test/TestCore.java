68c82807b17cfbf4b66c5f363ac1699bd7e8ae3b
evgeniy <evgeniy@takeit.to>
2015-09-09 19:32:56 +0300

    add UT @Before

--- test/TestCore.java
+++ test/TestCore.java
@@ -4,28 +4,27 @@ import org.junit.Before;
 
 public class TestCore {
 
-   /*  @Before
+    private Core testCore;
+
+    @Before
     public void setupTestData() {
-      Core testCore = Core.getInstance();
-    }*/
+        testCore = Core.getInstance();
+    }
 
 
     @Test
     public void testCreatingTestCore() {
-        Core testCore = Core.getInstance();
         assertNotNull("failture - object Core not created", testCore);
     }
 
     @Test
     public void testValidateUrl() {
-        Core testCore = Core.getInstance();
         assertTrue("failture - 'http://www.bbc.com' is invalid url.",testCore.validateUrl("http://www.bbc.com"));
         assertFalse("failture - 'unknow com' is valid url. ",testCore.validateUrl("unknow com"));
     }
 
     @Test
     public void testTypeOfGetPageContent() {
-        Core testCore = Core.getInstance();
         if (!(Core.getPageContent("http://www.bbc.com") instanceof String)) {
             fail("failture - getPageContent not return String from correct url");
         }
