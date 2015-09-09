46177bf7dbc33c6d71390d8c998b5c68cb51e080
evgeniy <evgeniy@takeit.to>
2015-09-09 09:20:29 +0300

    add UT for testCreatingTestCore()

--- test/TestCore.java
+++ test/TestCore.java
@@ -9,11 +9,23 @@ public class TestCore {
       Core testCore = Core.getInstance();
     }*/
 
+
+    @Test
+    public void testCreatingTestCore() {
+        Core testCore = Core.getInstance();
+        assertNotNull("Is object Core created?", testCore);
+    }
+
     @Test
     public void testValidateUrl() {
         Core testCore = Core.getInstance();
-        assertTrue("Is 'http://www.bbc.com valid' url. ",testCore.validateUrl("http://www.bbc.com"));
-        assertFalse("Is 'http://www.bbc.com invalid' url. ",testCore.validateUrl("unknow com"));
-
+        assertTrue("Is 'http://www.bbc.com valid' url? ",testCore.validateUrl("http://www.bbc.com"));
+        assertFalse("Is 'http://www.bbc.com invalid' url? ",testCore.validateUrl("unknow com"));
     }
+
+    /*@Test
+    public void testGetPageContent() {
+        Core testCore = Core.getInstance()
+
+    }*/
 }
